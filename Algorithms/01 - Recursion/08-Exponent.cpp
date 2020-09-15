fe52463828b5e6af8204263391866390722920b8
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-09-15 21:21:56 +0530

    add recursion example

--- Algorithms/01 - Recursion/08-Exponent.cpp
+++ Algorithms/01 - Recursion/08-Exponent.cpp
@@ -13,7 +13,20 @@ int power(int n, int pow) {
   }
 }
 
+int fast_power(int n, int pow) {
+  x++;
+  if(pow==0)
+    return 1;
+  if(pow%2==0)
+    return fast_power(n*n, pow/2);
+  else
+    return fast_power(n*n,(pow-1)/2) * n;
+}
+
 int main() {
-  cout << x << "  " <<  power(2,5) << "   " << x << "\n";
+  cout <<  power(2,9) << "\n";
   cout << "The function was called " << x << " times\n";
+  x=0;
+  fast_power(2,9);
+  cout << "By faster method, function was called " << x << " times\n";
 }
