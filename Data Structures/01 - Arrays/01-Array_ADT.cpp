b0e9cfb3627c13429507a9f2cc01eb19f126e844
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-09-18 12:30:39 +0530

    Add the insert() method

---
+++ Data Structures/01 - Arrays/01-Array_ADT.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+struct Array {
+  int* A;
+  int size;
+  int length;
+};
+
+void fill(struct Array x, int n) {
+    for(int i=0;i <n; i++) {
+        int t;
+        cin >> t;
+        x.A[i] = t;
+    }
+}
+
+void display(struct Array x, int n) {
+    for(int i=0;i<n;i++) {
+        cout << x.A[i] << " ";
+    }
+    cout << endl;
+    return;
+}
+
+void insert(struct Array x, int el, int t) {
+    if(x.length == x.size)
+        cout << "Array has reached size limit. No more elements can be added\n";
+    else if(t>x.length)
+        cout << "Index does not lie in the array range\n";
+    else {
+        for(int i=x.length-1; i>=t;i--)
+            x.A[i+1] = x.A[i];
+        x.A[t] = el;
+        x.length++;
+        display(x,x.length);
+    }
+}
+
+int main() {
+    struct Array arr;
+    int n,m;
+
+    while(true) {
+        cout << "Enter length of array : ";
+        cin >> n;
+        cout << "Enter maximum size of the array : ";
+        cin >> m;
+        if(m<n)
+            cout << "Array length cannot exceed the maximum size\n";
+        else if(n<0 || m<0)
+            cout << "Negative values are not accepted\n";
+        else
+            break;
+   }
+
+    arr.length = n;
+    arr.size = m;
+    int *x = new int[100];
+    arr.A = x;
+
+    cout << "Enter elements of the array : ";
+    fill(arr, n);
+    display(arr, n);
+
+    int el, idx;
+    cout << "Enter element to add : ";
+    cin >> el;
+    cout << "Enter index : ";
+    cin >> idx;
+    insert(arr, el, idx);
+
+    return 0;
+}
