f952cfd2d1d5f7c40768b7b403bb30e88880add8
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-12-11 10:34:29 +0530

    add Bubble sort.cpp

--- Algorithms/04 - Sorting/01-Bubble_sort.cpp
+++ Algorithms/04 - Sorting/01-Bubble_sort.cpp
@@ -8,7 +8,7 @@ void print(int arr[], int n){
 
 void bubble_sort(int arr[], int n) {
     for(int i=0; i<n; i++) {
-        for(int j=i+1; j<n; j++) {
+        for(int j = i+1; j < n; j++) {
             if(arr[i] > arr[j])
                 swap(arr[i], arr[j]);
         }
