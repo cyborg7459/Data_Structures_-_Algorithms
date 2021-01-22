fad546e0e3fb13c4ae1ce3b07923f201741a99cf
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-01-22 19:10:23 +0530

    implement insertion to min and max heap

--- Data Structures/08 - Heap/01-Inserting_in_heap.cpp
+++ Data Structures/08 - Heap/01-Inserting_in_heap.cpp
@@ -1,19 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Max_Heap {
-private:
-    std::vector<int> v;
+class Heap {
 public:
-    void insert(int x);
+    std::vector<int> v;
     void print();
 };
 
-void Max_Heap::print() {
+class Max_Heap:public Heap {
+public:
+    Max_Heap(std::vector<int> arr);
+    void insert(int x);
+};
+
+class Min_Heap:public Heap {
+public:
+    Min_Heap(std::vector<int> arr);
+    void insert(int x);
+};
+
+void Heap::print() {
     int n = v.size();
     for(int i=0; i<n; i++)
         cout << v[i] << " ";
     cout << endl;
+}
+
+Max_Heap::Max_Heap(std::vector<int> arr) {
+    int n = arr.size();
+    v.push_back(arr[0]);
+    for(int i=1; i<n; i++)
+        v.insert(arr[i]);
 }
 
 void Max_Heap::insert(int x) {
@@ -28,13 +45,31 @@ void Max_Heap::insert(int x) {
         }
         v[i-1] = x;
     }
-    print();
+}
+
+void Min_Heap::insert(int x) {
+    v.push_back(x);
+    if(v.size() == 1)
+        return;
+    else {
+        int i = v.size();
+        while(i>1 && v[i-1] < v[i/2 - 1]) {
+            v[i-1] = v[i/2 - 1];
+            i/=2;
+        }
+        v[i-1] = x;
+    }
 }
 
 int main() {
-    Max_Heap heap;
-    heap.insert(10);
-    heap.insert(4);
-    heap.insert(40);
-    heap.print();
+    Max_Heap maxHeap;
+    Min_Heap minHeap;
+    maxHeap.insert(10);
+    maxHeap.insert(4);
+    maxHeap.insert(40);
+    maxHeap.print();
+    minHeap.insert(10);
+    minHeap.insert(4);
+    minHeap.insert(40);
+    minHeap.print();
 }
