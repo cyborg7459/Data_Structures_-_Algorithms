df35de89df8782b4e476ac9cd5a5c6f4e49b9b49
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-11 12:00:19 +0530

    add atcoder dp problem 4

--- Algorithms/07 - Dynamic Programming/012-Atcoder_dp4_Knapsack1.cpp
+++ Algorithms/07 - Dynamic Programming/012-Atcoder_dp4_Knapsack1.cpp
@@ -1,29 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n, capacity;
-    cin >> n >> capacity;
-    pair<int, long long> weights[n], dp[capacity+1] = {};
-    for(int i=0; i<n; i++) {
-        int w, v;
-        cin >> w >> v;
-        weights[i] = {w, v};
-    }
-    dp[0] = {1, 0};
-    for(int i=0; i<n; i++) {
-        for(int x = capacity; x>=0; x--) {
-            if(dp[x].first == 1 && x + weights[i].first <= capacity) {
-                dp[x + weights[i].first].first = 1;
-                dp[x + weights[i].first].second = max(dp[x + weights[i].first].second, dp[x].second + weights[i].second);
-            }
-        }
-    }
-    long long mx = -1;
-    for(int i=0; i<=capacity; i++) {
-        // cout << i << " : " << dp[i].first << " , " << dp[i].second << "\n";
-        if(dp[i].first == 1)
-            mx = max(mx, dp[i].second);
-    }
-    cout << mx << "\n";
+#define int long long
+#define INF 1000000007
+
+signed main()
+{
+	int n, capacity;
+	cin >> n >> capacity;
+	pair<int, int> weights[n], dp[capacity+1] = {};
+	for(int i=0; i<n; i++) {
+		int w, v;
+		cin >> w >> v;
+		weights[i] = {w, v};
+	}
+	dp[0].first = 1;
+	dp[0].second = 0;
+	for(int i=0; i<n; i++) {
+		for(int j = capacity; j>=0; j--) {
+			if(j-weights[i].first >=0 && dp[j - weights[i].first].first == 1) {
+				dp[j].first = 1;
+				dp[j].second = max(dp[j].second, weights[i].second + dp[j - weights[i].first].second);
+			}
+		}
+	}
+
+	int mx = 0;
+	for(int i=0; i<=capacity; i++) {
+		if(dp[i].first==1 && dp[i].second > mx)
+			mx = dp[i].second;
+	}
+
+	cout << mx << "\n";
 }
