0ce49cae7b130568b5deba66e48790edeba711d6
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-11 14:00:21 +0530

    add dp problem 9

--- Algorithms/07 - Dynamic Programming/022-Atcoder_dp9_Coins.cpp
+++ Algorithms/07 - Dynamic Programming/022-Atcoder_dp9_Coins.cpp
@@ -5,58 +5,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pb push_back
-#define nl "\n"
 #define int long long
-#define loop(i,n) for(long long i=0;i<n;i++)
-#define print(x) {for(auto t : x)\
-                            cout << t << " ";\
-                            cout << "\n";}
-#define all(x) x.begin(), x.end()
-#define o(x) cout << x << "\n"
-#define fast ios_base::sync_with_stdio(false);\
-                     cin.tie(NULL);
 #define INF 1000000007
-
-void solve() {
-	int n;
-	cin >> n;
-	vector<double> prob;
-	loop(i, n) {
-		double d;
-		cin >> d;
-		prob.pb(d);
-	}
-	double dp[n+1][n+1] = {};
-	dp[0][0] = 1;
-	for(int i=1; i<=n; i++) {
-		for(int j=0; j<=i; j++) {
-			double p = 0;
-			if(i != j) {
-				double d1 = dp[i-1][j];
-				double d2 = 1 - prob[i-1];
-				p += (d1*d2);
-			}
-			if(j-1 >=0) {
-				double d1 = dp[i-1][j-1];
-				double d2 = prob[i-1];
-				p += (d1*d2);
-			}
-			dp[i][j] = p;
-		}
-	}
-	double ans = 0;
-	for(int i=ceil((double)n/2); i<=n; i++) {
-		ans += dp[n][i];
-	}
-	cout << setprecision(12) << ans << nl;
-}
 
 signed main()
 {
-    fast;
-    int q = 1;
-//	cin >> q;
-    while(q--)
-        solve();
+	cout << fixed << setprecision(12);
+	int n;
+	cin >> n;
+	double prob[n];
+	for(int i=0; i<n; i++)
+		cin >> prob[i];
+	double dp[n+1][n+1] = {};
+	for(int i=0; i<=n; i++)
+		dp[0][i] = 0;
+	dp[0][0] = 1;
+	double prod = 1;
+	for(int i=1; i<=n; i++) {
+		prod *= (1 - prob[i-1]);
+		dp[i][0] = prod;
+	}
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			if(j > i) dp[i][j] = 0;
+			else {
+				double p = prob[i-1];
+				dp[i][j] = p * dp[i-1][j-1] + (1-p)*dp[i-1][j];
+			}
+		}
+	}
+	double ans = 0;
+	for(int i = n/2 + 1; i<=n; i++) {
+		ans += dp[n][i];
+	}
+	cout << ans << "\n";
 }
