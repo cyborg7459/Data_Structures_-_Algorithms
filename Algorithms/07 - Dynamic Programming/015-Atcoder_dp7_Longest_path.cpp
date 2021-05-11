7c471aa617d09c7d68390fe7a93627d022e35b65
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-11 13:44:08 +0530

    add atcoder problem 7

--- Algorithms/07 - Dynamic Programming/015-Atcoder_dp7_Longest_path.cpp
+++ Algorithms/07 - Dynamic Programming/015-Atcoder_dp7_Longest_path.cpp
@@ -1,53 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define pb push_back
-#define nl "\n"
 #define int long long
-#define loop(i,n) for(long long i=0;i<n;i++)
-#define print(x) {for(auto t : x)\
-                            cout << t << " ";\
-                            cout << "\n";}
-#define all(x) x.begin(), x.end()
-#define o(x) cout << x << "\n"
-#define fast ios_base::sync_with_stdio(false);\
-                     cin.tie(NULL);
 #define INF 1000000007
+int choices[3000][3000] = {};
+int dp[3000][3000] = {};
 
-int longestPath(int cur, map<int, vector<int>> &mp, vector<int> &dp) {
-	if(mp[cur].size() == 0) return 0;
-	if(dp[cur] != -1) return dp[cur];
-	int ans = -1;
-	loop(i, mp[cur].size()) {
-		ans = max(ans, longestPath(mp[cur][i], mp, dp));
-	}
-	dp[cur] = 1+ans;
-	return dp[cur];
-}
-
-void solve() {
-	int n, m;
-	cin >> n >> m;
-	map<int, vector<int>> mp;
-	vector<int> dp(n+1, -1);
-	loop(i, n) mp.insert({i+1, {}});
-	loop(i, m) {
-		int a, b;
-		cin >> a >> b;
-		mp[a].pb(b);
-	}
-	int res = -1;
-	loop(i, n) {
-		res = max(res, longestPath(i+1, mp, dp));
-	}
-	o(res);
+int longestPath(int start, map<int, vector<int>> &mp, vector<int> &dp) {
+	if(dp[start] != -1) return dp[start];
+	if(mp[start].size() == 0) return 1;
+	int best = 0;
+	for(auto t : mp[start])
+		best = max(best, 1 + longestPath(t, mp, dp));
+	dp[start] = best;
+	return best;
 }
 
 signed main()
 {
-    fast;
-    int q = 1;
-//	cin >> q;
-    while(q--)
-        solve();
+	int n, m;
+	cin >> n >> m;
+	map <int, vector<int>> mp;
+	for(int i=1; i<=n; i++)
+		mp.insert({i, {}});
+	for(int i=0; i<m; i++) {
+		int a, b;
+		cin >> a >> b;
+		mp[a].push_back(b);
+	}
+	vector<int> dp(n+1, -1);
+	int ans = 0;
+	for(int i=1; i<n+1; i++) {
+		ans = max(ans, longestPath(i, mp, dp));
+	}
+	cout << ans-1 << "\n";
 }
