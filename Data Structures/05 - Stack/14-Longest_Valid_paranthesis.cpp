f551e8165d3512d0fce6286e2b8cf5a3d9b2dbcc
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-05-19 17:47:37 +0530

    add better implementation of longest_valid_parantesis

--- Data Structures/05 - Stack/14-Longest_Valid_paranthesis.cpp
+++ Data Structures/05 - Stack/14-Longest_Valid_paranthesis.cpp
@@ -1,36 +1,40 @@
 // PS : We have to calculate the maximum length of a valid bracket sequence in a given string
-// Solution : Create an extra array which keeps marking the indexes that lie in a valid sequence as 1, and all other elements as zero. At the end we just need to find out the longest continuous
-// subsequence of 1s in the array, which is easy to find
+// Solution : Checks the array once from the left, and then from the right for the number of right-opening brackets and the number of left-opening brackets and updating the value
+// of ans as soon as a valid sequence is obtained
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxLength(string S){
-    int n = S.length();
-    int arr[n] = {};
-    stack<pair<char, int>> st;
-    for(int i=0; i<n; i++) {
-        char c = S[i];
-        if(c == '(')
-            st.push({c, i});
-        else {
-            if(!st.empty()) {
-                arr[st.top().second] = 1;
-                arr[i] = 1;
-                st.pop();
+int maxLength(string s){
+    int l=0, r=0;
+        int ans = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(s[i]=='(')
+                l++;
+            else
+                r++;
+            if(l == r)
+                ans = max(ans, 2*r);
+            if(r > l) {
+                l = 0;
+                r = 0;
             }
         }
-    }
-    int ans = 0, cur = 0;
-    for(int i=0; i<n; i++) {
-        if(arr[i] == 1) cur++;
-        else {
-            ans = max(ans, cur);
-            cur = 0;
+        l = 0;
+        r = 0;
+        for(int i=s.length()-1; i>=0; i--) {
+            if(s[i] == '(')
+                l++;
+            else
+                r++;
+            if(l == r)
+                ans = max(ans, 2*r);
+            if(l > r) {
+                l = 0;
+                r = 0;
+            }
         }
-    }
-    ans = max(ans, cur);
-    return ans;
+        return ans;
 }
 
 int main() {
