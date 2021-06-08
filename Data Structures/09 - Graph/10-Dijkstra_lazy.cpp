f9c2cb18425802a554ed7219f46ecd7107203419
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-06-08 13:45:46 +0530

    add implementation of Dijkstra's algorithm

--- Data Structures/09 - Graph/10-Dijkstra_lazy.cpp
+++ Data Structures/09 - Graph/10-Dijkstra_lazy.cpp
@@ -1,7 +1,16 @@
+// Dijksrta's algorithm is used to find the shortest path from the source node to each node in the graph.
+// It works in O((n+e)logn) time
+// Dijkstra's is based on the principle that once we visit a given node, we definitely put in its minimum value. Hence, this algorithm fails when the edges have negative values (if we remove
+// the visited array then we can get correct answer, but that would decrease the efficiency of the algorithm)
+// How we can say that once the node is visited, it is definitely the correct answer : Suppose we have an element waiting in the queue, if there is some other path better than that path in the
+// queue, then definitely it will be added into the queue before that element is accessed. Not only that, it would be accessed first after addition as well since the priority_queue is
+// implemented in such a way that shorter paths are popped off first.
+
 #include <bits/stdc++.h>
 using namespace std;
 #define inf 1000000007
 vector<pair<int, int>> adj[101];
+bool vis[101] = {};
 
 vector<int> Dijkstras(int source, int n) {
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
@@ -12,8 +21,10 @@ vector<int> Dijkstras(int source, int n) {
         pair<int, int> cur = pq.top();
         pq.pop();
         int cur_dist = cur.first, cur_node = cur.second;
+        vis[cur_node] = true;
         if(dist[cur_node] < cur_dist) continue;
         for(pair<int, int> node : adj[cur_node]) {
+            if(vis[node.first]) continue;
             int new_dist = cur_dist + node.second;
             if(new_dist < dist[node.first]) {
                 dist[node.first] = new_dist;
