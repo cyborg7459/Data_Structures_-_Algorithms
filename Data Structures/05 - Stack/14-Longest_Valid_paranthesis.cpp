e6bdb100ae7441a2971320b8e6e3b178ce5193bb
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-06-23 17:44:27 +0530

    .

--- Data Structures/05 - Stack/14-Longest_Valid_paranthesis.cpp
+++ Data Structures/05 - Stack/14-Longest_Valid_paranthesis.cpp
@@ -7,34 +7,34 @@ using namespace std;
 
 int maxLength(string s){
     int l=0, r=0;
-        int ans = 0;
-        for(int i=0; i<s.length(); i++) {
-            if(s[i]=='(')
-                l++;
-            else
-                r++;
-            if(l == r)
-                ans = max(ans, 2*r);
-            if(r > l) {
-                l = 0;
-                r = 0;
-            }
+    int ans = 0;
+    for(int i=0; i<s.length(); i++) {
+        if(s[i]=='(')
+            l++;
+        else
+            r++;
+        if(l == r)
+            ans = max(ans, 2*r);
+        if(r > l) {
+            l = 0;
+            r = 0;
         }
-        l = 0;
-        r = 0;
-        for(int i=s.length()-1; i>=0; i--) {
-            if(s[i] == '(')
-                l++;
-            else
-                r++;
-            if(l == r)
-                ans = max(ans, 2*r);
-            if(l > r) {
-                l = 0;
-                r = 0;
-            }
+    }
+    l = 0;
+    r = 0;
+    for(int i=s.length()-1; i>=0; i--) {
+        if(s[i] == '(')
+            l++;
+        else
+            r++;
+        if(l == r)
+            ans = max(ans, 2*r);
+        if(l > r) {
+            l = 0;
+            r = 0;
         }
-        return ans;
+    }
+    return ans;
 }
 
 int main() {
