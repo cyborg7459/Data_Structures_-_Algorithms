3f5663e89d2e1f08b949a5e29745cd91ace09b60
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-06-30 13:43:17 +0530

    add array implementation of min and max heaps

--- Data Structures/08 - Heap/01-Array_implementation.cpp
+++ Data Structures/08 - Heap/01-Array_implementation.cpp
@@ -3,26 +3,26 @@ using namespace std;
 
 class Heap {
 public:
-    std::vector<int> v;
+    vector<int> v;
     void print();
     int size();
 };
 
 void Heap::print() {
-    int n = v.size();
-    for(int i=0; i<n; i++)
-    cout << v[i] << " ";
+    for(int t : v)
+        cout << t << " ";
     cout << endl;
 }
 
 int Heap::size() {
-    return v.size();
+    int n = v.size();
+    return n;
 }
 
 class Max_Heap:public Heap {
 public:
     Max_Heap() {};
-    Max_Heap(std::vector<int> arr);
+    Max_Heap(vector<int> _v);
     void insert(int x);
     int delete_root();
 };
@@ -30,118 +30,113 @@ public:
 class Min_Heap:public Heap {
 public:
     Min_Heap() {};
-    Min_Heap(std::vector<int> arr);
+    Min_Heap(vector<int> _v);
     void insert(int x);
     int delete_root();
 };
 
-Max_Heap::Max_Heap(std::vector<int> arr) {
-    int n = arr.size();
-    for(int i=0; i<n; i++)
-        insert(arr[i]);
-}
-
-Min_Heap::Min_Heap(std::vector<int> arr) {
-    int n = arr.size();
-    for(int i=0; i<n; i++)
-        insert(arr[i]);
-}
-
 void Max_Heap::insert(int x) {
     v.push_back(x);
-    if(v.size() == 1)
-        return;
-    else {
-        int i = v.size();
-        while(i>1 && v[i-1] > v[i/2 - 1]) {
-            v[i-1] = v[i/2 - 1];
-            v[i/2 - 1] = x;
-            i/=2;
-        }
-        v[i-1] = x;
+    int i = v.size() - 1;
+    while(i != 0 && x > v[(i-1)/2]) {
+        v[i] = v[(i-1)/2];
+        v[(i-1)/2] = x;
+        i = (i-1)/2;
     }
-}
-
-int Max_Heap::delete_root() {
-    if(v.size() == 0)
-        return -1;
-    int deleted = v[0], num = v[v.size()-1];
-    int sz = v.size();
-    v.pop_back();
-    v[0] = num;
-    int i = 1;
-    while(true) {
-        int lchild = 2*i;
-        int maxi = lchild;
-        if(lchild > sz)
-            break;
-        int rchild = 2*i + 1;
-        if(rchild <= sz && v[rchild-1]>v[lchild-1])
-            maxi = rchild;
-        if(v[maxi-1] > num) {
-            v[i-1] = v[maxi-1];
-            v[maxi-1] = num;
-            i = maxi;
-        }
-        else
-            break;
-    }
-    return deleted;
+    v[i] = x;
+    return;
 }
 
 void Min_Heap::insert(int x) {
     v.push_back(x);
-    if(v.size() == 1)
-        return;
-    else {
-        int i = v.size();
-        while(i>1 && v[i-1] < v[i/2 - 1]) {
-            v[i-1] = v[i/2 - 1];
-            v[i/2 - 1] = x;
-            i/=2;
-        }
-        v[i-1] = x;
+    int i = size()-1;
+    while(i != 0 && x < v[(i-1)/2]) {
+        v[i] = v[(i-1)/2];
+        v[(i-1)/2] = x;
+        i = (i-1)/2;
     }
+    v[i] = x;
+    return;
+}
+
+int Max_Heap::delete_root() {
+    if(size() == 0) return -1;
+    int num = v[0], x = v[size()-1];
+    v.pop_back();
+    int i = 0;
+    v[i] = x;
+    while(true) {
+        int i1 = 2*i + 1, i2 = 2*i + 2;
+        if(i1 >= size()) break;
+        else if(i1 == size()-1 && v[i1] > x) {
+            v[i] = v[i1];
+            v[i1] = x;
+            i = i1;
+        }
+        else {
+            int idx;
+            if(v[i1] > v[i2]) idx = i1;
+            else idx = i2;
+            if(x >= v[idx]) break;
+            v[i] = v[idx];
+            v[idx] = x;
+            i = idx;
+        }
+    }
+    v[i] = x;
+    return num;
 }
 
 int Min_Heap::delete_root() {
-    if(v.size() == 0)
-    return -1;
-    int deleted = v[0], num = v[v.size()-1];
-    int sz = v.size();
+    if(size() == 0) return -1;
+    int num = v[0], x = v[size()-1];
     v.pop_back();
-    v[0] = num;
-    int i = 1;
+    int i = 0;
+    v[i] = x;
     while(true) {
-        int lchild = 2*i;
-        int maxi = lchild;
-        if(lchild > sz)
-        break;
-        int rchild = 2*i + 1;
-        if(rchild <= sz && v[rchild-1]<v[lchild-1])
-        maxi = rchild;
-        if(v[maxi-1] < num) {
-            v[i-1] = v[maxi-1];
-            v[maxi-1] = num;
-            i = maxi;
+        int i1 = 2*i + 1, i2 = 2*i + 2;
+        if(i1 >= size()) break;
+        else if(i1 == size()-1 && v[i1] < x) {
+            v[i] = v[i1];
+            v[i1] = x;
+            i = i1;
         }
-        else
-            break;
+        else {
+            int idx;
+            if(v[i1] < v[i2]) idx = i1;
+            else idx = i2;
+            if(x <= v[idx]) break;
+            v[i] = v[idx];
+            v[idx] = x;
+            i = idx;
+        }
     }
-    return deleted;
+    v[i] = x;
+    return num;
+}
+
+Max_Heap::Max_Heap(vector<int> _v) {
+    for(int t : _v)
+        insert(t);
+}
+
+Min_Heap::Min_Heap(vector<int> _v) {
+    for(int t : _v)
+        insert(t);
 }
 
 int main() {
-    Max_Heap maxHeap({5,25,10,15,30,35,40});
-    Min_Heap minHeap({40,35,30,15,10,25,5});
-    maxHeap.print();
-    while(maxHeap.size() > 0) {
-        maxHeap.delete_root();
-        maxHeap.print();
-    }
-    minHeap.print();
-    while(minHeap.size() > 0) {
-        minHeap.delete_root();
-        minHeap.print();
-    }
+	vector<int> v = {4,2,7,1,15,32,14,8,11,17,5};
+	Max_Heap maxh(v);
+	while(maxh.size()) {
+		int t = maxh.delete_root();
+		cout << t << " ";
+	}
+	cout << endl;
+	Min_Heap minh(v);
+	while(minh.size()) {
+	 int t = minh.delete_root();
+	 cout << t << " ";
+	}
+	cout << endl;
 }
