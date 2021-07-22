3f62f153b46288206e2c390a6d8fe1457bc8e23b
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-07-22 13:56:18 +0530

    add stock_buy_and_sell variant2

--- Algorithms/07 - Dynamic Programming/059-Stock_buy_and_sell_all_variants.cpp
+++ Algorithms/07 - Dynamic Programming/059-Stock_buy_and_sell_all_variants.cpp
@@ -6,7 +6,7 @@
 // Explanation : We simply need to maximize the difference between the price of stock on a day and the minimum price
 // of stock in all the days preceding that day
 
-int maxProfit1(vector<int>& prices) {
+int maxProfit(vector<int>& prices) {
     if(prices.size() == 0) return 0;
     int hold = prices[0], cash = 0;
     for(int i=0; i<prices.size(); i++) {
@@ -17,3 +17,34 @@ int maxProfit1(vector<int>& prices) {
 }
 
 // ==================================================================================================================
+// VARIANT 2 : WE CAN MAKE AS MANY TRANSACTIONS AS WE WISH
+// Explanation : Given problem needs dynamic programming. We start from the first day and keep in track wheter we own
+// a stock or not. In case we own a stock, we can either choose to keep it or sell it on any particular day. In case
+// we do not own a stock, we can choose to either buy stock or do nothing on any particular day. We have to maxumize
+// these values using dynamic programming
+
+int dp[100005][2];
+
+int func(int i, bool own, const vector<int>&v) {
+    if(i == v.size()) return 0;
+    if(own) {
+        if(dp[i][1] != -1) return dp[i][1];
+        int opt1 = func(i+1, true, v);
+        int opt2 = v[i] + func(i+1, false, v);
+        dp[i][1] = max(opt1, opt2);
+        return dp[i][1];
+    }
+    else {
+        if(dp[i][0] != -1) return dp[i][0];
+        int opt1 = func(i+1, false, v);
+        int opt2 = func(i+1, true, v) - v[i];
+        dp[i][0] = max(opt1, opt2);
+        return dp[i][0];
+    }
+}
+
+int maxProfit(const vector<int> &A) {
+    memset(dp, -1, sizeof dp);
+    if(A.size() == 0) return 0;
+    return func(0, false, A);
+}
