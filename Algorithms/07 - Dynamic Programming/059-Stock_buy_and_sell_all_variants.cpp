731b602287f3ccc155dbb36a197c52b72569cdc5
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-07-22 14:04:43 +0530

    add stock_buy_and_sell variant3

--- Algorithms/07 - Dynamic Programming/059-Stock_buy_and_sell_all_variants.cpp
+++ Algorithms/07 - Dynamic Programming/059-Stock_buy_and_sell_all_variants.cpp
@@ -48,3 +48,36 @@ int maxProfit(const vector<int> &A) {
     if(A.size() == 0) return 0;
     return func(0, false, A);
 }
+
+// =====================================================================================================================
+// VARIANT 3 : WE CAN MAKE AS MANY TRANSACTIONS AS WE WISH BUT EACH TRANSACTION HAS AN ASSOCIATED FEE
+// Explanation : Implementation same as last one with modification in the values, as we include the transaction fee as
+// well while selling the stock
+
+int dp[50005][2];
+
+int helper(int day, bool own, vector<int> &prices, int fee) {
+    if(day == prices.size()) return 0;
+    if(own) {
+        if(dp[day][1] != -1) return dp[day][1];
+        int opt1 = prices[day] - fee + helper(day+1, false, prices, fee);
+        int opt2 = helper(day+1, true, prices, fee);
+        int chosen = max(opt1, opt2);
+        dp[day][1] = chosen;
+        return chosen;
+    }
+    else {
+        if(dp[day][0] != -1) return dp[day][0];
+        int opt1 = helper(day+1, false, prices, fee);
+        int opt2 = helper(day+1, true, prices, fee) - prices[day];
+        int chosen = max(opt1, opt2);
+        dp[day][0] = chosen;
+        return chosen;
+    }
+}
+
+int maxProfit(vector<int>& prices, int fee) {
+    memset(dp, -1, sizeof dp);
+    if(prices.size() == 0) return 0;
+    return helper(0, false, prices, fee);
+}
