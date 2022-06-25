688a2c7c94301ad5ee5e7ed6d26909029696a419
Shreyash Singh <shreyashsingh17@gmail.com>
2022-06-25 12:16:55 +0530

    add merge sort to linked list

--- Data Structures/04 - Linked List/25-Merge_sort_LinkedList.cpp
+++ Data Structures/04 - Linked List/25-Merge_sort_LinkedList.cpp
@@ -1,100 +1,98 @@
+// { Driver Code Starts
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-public:
+struct Node {
     int data;
-    Node* next;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
 };
 
-Node* SortedMerge(Node* a, Node* b);
-void FrontBackSplit(Node* source, Node** frontRef, Node** backRef);
-
-void MergeSort(Node** headRef)
-{
-    Node* head = *headRef;
-    Node* a;
-    Node* b;
-    if ((head == NULL) || (head->next == NULL)) {
-        return;
-    }
-    FrontBackSplit(head, &a, &b);
-
-    MergeSort(&a);
-    MergeSort(&b);
-    *headRef = SortedMerge(a, b);
-}
-
-Node* SortedMerge(Node* a, Node* b)
-{
-    Node* result = NULL;
-    if (a == NULL)
-        return (b);
-    else if (b == NULL)
-        return (a);
-    if (a->data <= b->data) {
-        result = a;
-        result->next = SortedMerge(a->next, b);
-    }
-    else {
-        result = b;
-        result->next = SortedMerge(a, b->next);
-    }
-    return (result);
-}
-
-void FrontBackSplit(Node* source, Node** frontRef, Node** backRef)
-{
-    Node* fast;
-    Node* slow;
-    slow = source;
-    fast = source->next;
-    while (fast != NULL) {
-        fast = fast->next;
-        if (fast != NULL) {
-            slow = slow->next;
-            fast = fast->next;
+void merge(Node* &start, Node* &end, Node* &mid) {
+    vector<int> v = {};
+    Node* n1 = start;
+    Node* n2 = mid->next;
+    while(n1 && n1!=mid->next && n2 && n2!=end->next) {
+        if(n1->data < n2->data) {
+            v.push_back(n1->data);
+            n1 = n1->next;
+        }
+        else {
+            v.push_back(n2->data);
+            n2 = n2->next;
         }
     }
+    while(n1 && n1!=mid->next) {
+        v.push_back(n1->data);
+        n1 = n1->next;
+    }
+    while(n2 && n2!=end->next) {
+        v.push_back(n2->data);
+        n2 = n2->next;
+    }
 
-    *frontRef = source;
-    *backRef = slow->next;
-    slow->next = NULL;
-}
-
-/* Function to print nodes in a given linked list */
-void printList(Node* node)
-{
-    while (node != NULL) {
-        cout << node->data << " ";
+    Node* node = start;
+    for(int i=0; i<v.size(); i++) {
+        node->data = v[i];
         node = node->next;
     }
 }
 
-/* Function to insert a node at the beginging of the linked list */
-void push(Node** head_ref, int new_data)
-{
-    Node* new_node = new Node();
-    new_node->data = new_data;
+void mergeSortFunc(Node* &start, Node* &end) {
+    if(start == end) return;
+    Node* mid = start;
+    Node* fast = start;
+
+    while(fast && fast != end) {
+        if(!fast->next) break;
+        fast = fast->next;
+        if(fast == end) break;
+        mid = mid->next;
+        if(fast && fast->next) fast = fast->next;
+        else break;
+    }
+
+    mergeSortFunc(start, mid);
+    mergeSortFunc(mid->next, end);
+    merge(start, end, mid);
+}
+
+Node* mergeSort(Node* head) {
+    Node* start = head;
+    Node* end = head;
+    while(end->next) end = end->next;
+    mergeSortFunc(head, end);
+    return head;
+}
+
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+void push(struct Node** head_ref, int new_data) {
+    Node* new_node = new Node(new_data);
     new_node->next = (*head_ref);
     (*head_ref) = new_node;
 }
 
-/* Driver program to test above functions*/
-int main()
-{
-    Node* a = NULL;
-
-    push(&a, 15);
-    push(&a, 10);
-    push(&a, 5);
-    push(&a, 20);
-    push(&a, 3);
-    push(&a, 2);
-
-    MergeSort(&a);
-    cout << "Sorted Linked List is: \n";
-    printList(a);
-
-    return 0;
+int main() {
+  struct Node* a = NULL;
+  long n, tmp;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+      cin >> tmp;
+      push(&a, tmp);
+  }
+  a = mergeSort(a);
+  printList(a);
 }
